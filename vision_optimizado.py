d31f1ad4426ec478fc7cdaddecbf450f4e8067c5
Eloy <eurriens@gmail.com>
2025-12-04 10:20:37 +0100

    Refactor image processing and prediction classes

--- vision_optimizado.py
+++ vision_optimizado.py
@@ -3,19 +3,22 @@ import os
 import cv2
 import random
 
-from typing import Tuple, List
+from typing import Tuple, List, Literal
 from abc import ABC, abstractmethod
 from numba import njit
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 # ======== SISTEMA DE LECTURA DE DATOS ========
 class ClasePerroGato:
-    CLASE_PERRO = 0
+    CLASE_PERRO = -1
     CLASE_GATO = 1
 
 class Lector:
     def __init__(self, ruta_train_perros: str, ruta_train_gatos: str,
                 ruta_test_perros: str, ruta_test_gatos: str):
-        self.__ruta_train_perros = ruta_train_perros # El __ es para poner los miembros privados
+        self.__ruta_train_perros = ruta_train_perros
         self.__ruta_train_gatos = ruta_train_gatos
         self.__ruta_test_perros = ruta_test_perros
         self.__ruta_test_gatos = ruta_test_gatos
@@ -61,52 +64,135 @@ class Lector:
         return imagenes
 
 # ======== SISTEMA DE PREPROCESAMIENTO ========
+@njit
+def conv_media_preprocesador(img_amp: np.ndarray, m: int) -> np.ndarray:
+    resultado = np.zeros(shape=(img_amp.shape[0] - m + 1, img_amp.shape[1] - m + 1), dtype=np.float32)
+    for i in range(img_amp.shape[0] - m):
+        for j in range(img_amp.shape[1] - m):
+            zona_comun = img_amp[i:i+m, j:j+m]
+            resultado[i, j] = np.mean(zona_comun)
+    return resultado.astype(np.uint8)
+
+@njit
+def conv_mediana_preprocesador(img_amp: np.ndarray, m: int) -> np.ndarray:
+    resultado = np.zeros(shape=(img_amp.shape[0] - m + 1, img_amp.shape[1] - m + 1), dtype=np.float32)
+    for i in range(img_amp.shape[0] - m):
+        for j in range(img_amp.shape[1] - m):
+            zona_comun = img_amp[i:i+m, j:j+m]
+            resultado[i, j] = np.median(zona_comun)
+    return resultado.astype(np.uint8)
+
+@njit
+def conv_kernel_preprocesador(img_amp: np.ndarray, m: int, kernel: np.ndarray) -> np.ndarray:
+    resultado = np.zeros(shape=(img_amp.shape[0] - m + 1, img_amp.shape[1] - m + 1), dtype=np.float32)
+    for i in range(img_amp.shape[0] - m):
+        for j in range(img_amp.shape[1] - m):
+            zona_comun = img_amp[i:i+m, j:j+m]
+            resultado[i, j] = np.sum(kernel * zona_comun)
+    return resultado.clip(0, 255).astype(np.uint8)
+
 class Preprocesador(ABC):
-    def __init__(self):
-        pass
-
-    def __eliminar_ruido_media(self, img: np.ndarray, m: int) -> np.ndarray:
-        pass
-
-    def __eliminar_ruido_mediana(self, img: np.ndarray, m: int) -> np.ndarray:
-        pass
-
-    def __equalizar(self, img: np.ndarray) -> np.ndarray:
-        pass
+    def __init__(self, m: int):
+        if m % 2 == 0 or m <= 0:
+            raise Exception('El tamaño del kernel tiene que ser impar y positivo')
+        
+        self.__m = m
 
     @abstractmethod
     def preprocesar(self, img: np.ndarray) -> np.ndarray:
         pass
 
-class PrepMedia(Preprocesador):
-    def __init__(self):
-        super().__init__()
+    def filtro_media(self, img: np.ndarray) -> np.ndarray:
+        return self.__aplicar_filtro(img, 'media')
 
-        self.__TAM_KERNEL = 3
+    def filtro_mediana(self, img: np.ndarray) -> np.ndarray:
+        return self.__aplicar_filtro(img, 'mediana')
+
+    def filtro_gaussiano(self, img: np.ndarray, sigma: float) -> np.ndarray:
+        kernel = cv2.getGaussianKernel(self.__m, sigma) * cv2.getGaussianKernel(self.__m, sigma).T
+        return self.__aplicar_filtro(img, 'gaussiano', kernel)
+
+    def __aplicar_filtro(
+            self, img: np.ndarray,
+            tipo: Literal['media', 'mediana', 'gaussiano'],
+            kernel: np.ndarray | None=None
+        ) -> np.ndarray:
+
+        if not(tipo in ['media', 'mediana', 'gaussiano']):
+            raise Exception('Tipo de filtro no valido.')
+        if tipo == 'gaussiano' and kernel is None:
+            raise Exception('Es necesario especificar el kernel para el filtro gaussiano.')
+
+        mdiv2 = self.__m // 2
+        resultado = np.zeros(img.shape, dtype=np.uint8)
+        img_amp = cv2.copyMakeBorder(img, mdiv2, mdiv2, mdiv2, mdiv2, cv2.BORDER_REPLICATE)
+        for i in range(3):
+            if tipo == 'media':
+                resultado[:, :, i] = conv_media_preprocesador(img_amp[:, :, i], self.__m)
+            elif tipo == 'mediana':
+                resultado[:, :, i] = conv_mediana_preprocesador(img_amp[:, :, i], self.__m)
+            elif tipo == 'gaussiano':
+                resultado[:, :, i] = conv_kernel_preprocesador(img_amp[:, :, i], self.__m, kernel)
+
+        return resultado
+
+    def equalizar(self, img: np.ndarray) -> np.ndarray:
+        img_equ = np.zeros(shape=img.shape, dtype=np.uint8)
+        for i in range(3):
+            img_equ[:, :, i] = self.__equalizar_canal(img[:, :, i])
+        return img_equ
+
+    def __equalizar_canal(self, canal: np.ndarray) -> np.ndarray:
+        hist = np.bincount(canal.ravel(), minlength=256)
+        cumsum = hist.cumsum()
+        f_Ixy = (255 * (cumsum / cumsum[-1])).astype(np.uint8)
+        return f_Ixy[canal]
+        
+class PrepMedia(Preprocesador):
+    def __init__(self, m: int):
+        super().__init__(m)
 
     def preprocesar(self, img: np.ndarray) -> np.ndarray:
-        return self.__eliminar_ruido_media(img, self.__TAM_KERNEL)
+        return self.filtro_media(img)
 
 class PrepMediana(Preprocesador):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, m: int):
+        super().__init__(m)
 
     def preprocesar(self, img: np.ndarray) -> np.ndarray:
-        pass
+        return self.filtro_mediana(img)
+    
+class PrepGaussiano(Preprocesador):
+    def __init__(self, m: int, sigma: float):
+        super().__init__(m)
+        self.__sigma = sigma
+
+    def preprocesar(self, img: np.ndarray) -> np.ndarray:
+        return self.filtro_gaussiano(img, self.__sigma)
 
 class PrepMediaEqu(Preprocesador):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, m):
+        super().__init__(m)
 
     def preprocesar(self, img: np.ndarray) -> np.ndarray:
-        pass
-
+        filtrada = self.filtro_media(img)
+        return self.equalizar(filtrada)
 class PrepMedianaEqu(Preprocesador):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, m: int):
+        super().__init__(m)
 
     def preprocesar(self, img: np.ndarray) -> np.ndarray:
-        pass
+        filtrada = self.filtro_mediana(img)
+        return self.equalizar(filtrada)
+    
+class PrepGaussianoEqu(Preprocesador):
+    def __init__(self, m: int, sigma: float):
+        super().__init__(m)
+        self.__sigma = sigma
+
+    def preprocesar(self, img: np.ndarray) -> np.ndarray:
+        filtrada = self.filtro_gaussiano(img, self.__sigma)
+        return self.equalizar(filtrada)
 
 # ======== SISTEMA DE EXTRACCION DE CARACTERISTICAS ========
 @njit
@@ -117,7 +203,7 @@ def conv_algoritmo_texturas(img_amp: np.ndarray, m: int) -> np.ndarray:
         [2**1, 2**2, 2**3]
     ], dtype=np.uint8)
 
-    resultado = np.zeros(shape=(img_amp.shape[0] + m, img_amp.shape[1] + m), dtype=np.uint8)
+    resultado = np.zeros(shape=(img_amp.shape[0] - m + 1, img_amp.shape[1] - m + 1), dtype=np.uint8)
     for i in range(img_amp.shape[0] - m):
         for j in range(img_amp.shape[1] - m):
             zona_comun = img_amp[i:i+m, j:j+m]
@@ -305,26 +391,55 @@ class TransformadorCaracteristicas:
 
 # ======== SISTEMA DE IA ========
 class PredictorPerroGato:
-    def __init__(self, modelo):
+    def __init__(self, tipo_modelo: Literal['adaboost', 'random_forest'], max_est: int):
         ''' modelo: Un modelo de IA para entrenarlo y predecir '''
-        pass
+        if not(tipo_modelo in ['adaboost', 'random_forest']):
+            raise Exception('Tipo de modelo incorrecto')
+        if max_est < 2:
+            raise Exception('Requisito: Numero de estimadores >= 2')
+        
+        self.__max_est = max_est
+        self.__tipo_modelo = tipo_modelo
+        self.__modelo = None
 
-    def entrenar(self, X: np.ndarray, y: np.ndarray) -> None:
-        """ Hacer un entrenamiento buscando los mejores hiperparametros con conjunto de validacion """
-        pass
+    def entrenar(
+            self, X_train: np.ndarray,
+            y_train: np.ndarray,
+            X_val: np.ndarray,
+            y_val: np.ndarray
+        ) -> None:
+        
+        accuracies = []
+        best_acc = -np.inf
+        best_model = None
+        for n_est in range(2, self.__max_est + 1):
+            modelo = self.__crear_modelo(n_est)
+            modelo.fit(X_val, y_val)
 
-    def predecir_perro_gato(self, X: np.ndarray) -> np.ndarray:
-        pass
+            y_pred = modelo.predict(X_val)
+            acc = np.where(y_val == y_pred)[0].size / y_pred.shape[0]
+            if acc > best_acc:
+                best_acc = acc
+                best_model = modelo
+            accuracies.append(acc)
+        
+        self.__modelo = best_model
+        self.__modelo.fit(X_train, y_train)
+        return accuracies
 
-class PredictorAdaboost(PredictorPerroGato):
-    def __init__(self):
-        # modelo = (Creamos el Adaboost)
-        super().__init__(modelo)
-
-class PredictorRandomForest(PredictorPerroGato):
-    def __init__(self):
-        # modelo = (Creamos el RandomForest)
-        super().__init__(modelo)
+    def predecir(self, X: np.ndarray) -> np.ndarray:
+        if self.__modelo is None:
+            raise Exception("El modelo no esta entrenado.")
+        return self.__modelo.predict(X)
+    
+    def __crear_modelo(self, n_estimadores: int):
+        if self.__tipo_modelo == 'adaboost':
+            return AdaBoostClassifier(
+                estimator=DecisionTreeClassifier(max_depth=1),
+                n_estimators=n_estimadores
+            )
+        elif self.__tipo_modelo == 'random_forest':
+            return RandomForestClassifier(n_estimators=n_estimadores)
 
 def experimento(
         preprocesador: Preprocesador,
@@ -350,19 +465,19 @@ def experimento(
         X_test[i] = preprocesador.preprocesar(img)
 
     # Transformamos las imagenes a vectores de caracterisitcas
-    transformador = TransformadorCaracteristicas(algoritmo)
+    '''transformador = TransformadorCaracteristicas(algoritmo)
     for i, img in enumerate(X_train):
         X_train[i] = transformador.transformar(img)
     for i, img in enumerate(X_test):
-        X_test[i] = transformador.transformar(img)
+        X_test[i] = transformador.transformar(img)'''
 
     # Entrenar la IA
-    predictor.entrenar(X_train, y_train)
+    '''predictor.entrenar(X_train, y_train)
 
     # =========== PROCESO DE PREDICCION ===========
-    y_test_pred = predictor.predecir_perro_gato(X_test)
+    y_test_pred = predictor.predecir(X_test)
     accuracy = np.where(y_test == y_test_pred)[0].size / y_test.size
-    return accuracy
+    return accuracy'''
 
 if __name__ == '__main__':
-    experimento(PrepMedia(), AlgoritmoOrientaciones(), PredictorAdaboost())
+    experimento(PrepGaussianoEqu(7, 3), AlgoritmoTexturas(), PredictorPerroGato('random_forest', 100))
